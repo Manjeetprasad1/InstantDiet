f0bd0e114d4591c3c98ecd28d06fdf936e24b1c4
manny <manjeet.prasad@rootinfosol.com>
2023-02-08 21:32:39 +0530

    login screen

---
+++ app/src/main/java/com/manny/instantdiet/activity/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.manny.instantdiet.activity
+
+import android.os.Bundle
+import com.manny.instantdiet.BaseActivity
+import com.manny.instantdiet.R
+
+class LoginActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/manny/instantdiet/activity/SplashActivity.kt
+++ app/src/main/java/com/manny/instantdiet/activity/SplashActivity.kt
@@ -1,11 +1,24 @@
 package com.manny.instantdiet.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.manny.instantdiet.BaseActivity
+import com.manny.instantdiet.MainActivity
+import com.manny.instantdiet.R
+
 
 class SplashActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.myLooper()!!).postDelayed(Runnable { // This method will be executed once the timer is over
+            val i = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(i)
+            finish()
+        }, 3000)
     }
 }
