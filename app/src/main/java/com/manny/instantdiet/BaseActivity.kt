06f96be2dfb02263cc4650e36ba591e2e5703783
manny <manjeet.prasad@rootinfosol.com>
2023-02-08 14:17:27 +0530

    splash screen adn arch of code

---
+++ app/src/main/java/com/manny/instantdiet/BaseActivity.kt
@@ -0,0 +1,6 @@
+package com.manny.instantdiet
+
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity : AppCompatActivity(){
+}
---
+++ app/src/main/java/com/manny/instantdiet/activity/SplashActivity.kt
@@ -0,0 +1,11 @@
+package com.manny.instantdiet.activity
+
+import android.os.Bundle
+import com.manny.instantdiet.BaseActivity
+
+class SplashActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
