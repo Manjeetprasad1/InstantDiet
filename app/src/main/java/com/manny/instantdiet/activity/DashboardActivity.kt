be1034b29283a7eaf786f5fda56bf8e41a007619
manny <manjeet.prasad@rootinfosol.com>
2023-02-11 00:58:53 +0530

    dashbaord viewodel

--- app/src/main/java/com/manny/instantdiet/activity/DashboardActivity.kt
+++ app/src/main/java/com/manny/instantdiet/activity/DashboardActivity.kt
@@ -1,11 +1,20 @@
 package com.manny.instantdiet.activity
 
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.ViewModelProvider
 import com.manny.instantdiet.BaseActivity
+import com.manny.instantdiet.R
+import com.manny.instantdiet.databinding.ActivityDashboardBinding
+import com.manny.instantdiet.viewModel.DashboardViewModel
 
 class DashboardActivity : BaseActivity() {
-
+    private lateinit var binding: ActivityDashboardBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_dashboard)
+        binding.viewModel = DashboardViewModel()
+        binding.viewModel = ViewModelProvider(this).get(DashboardViewModel::class.java)
     }
 }
