f6cbaa433813c03d1f017f4b106f3d063a085f66
manny <manjeet.prasad@rootinfosol.com>
2023-02-23 08:28:51 +0530

    details screen changes

--- app/src/main/java/com/manny/instantdiet/activity/CheckoutActivity.kt
+++ app/src/main/java/com/manny/instantdiet/activity/CheckoutActivity.kt
@@ -2,6 +2,7 @@ package com.manny.instantdiet.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.manny.instantdiet.R
 
 class CheckoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
